6d43f338692cf4a23270056885829c931982d932
Peter Tountas <petertountas1@gmail.com>
2017-01-10 21:34:16 -0600

    better return on mysql query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,5 +11,8 @@ var connect = mysql.createConnection({
 
 
 connect.query("Select * FROM products", function(err,res){
-    console.log(res);
+    	console.log("\nProduct List\n")
+    for(var i=0; i <res.length; i++){
+    	console.log(res[i].item_id + " | "  + res[i].product_name + " | " + res[i].department_name +  " | " + res[i].price + " | " + res[i].stock_qty);
+    }
 });
