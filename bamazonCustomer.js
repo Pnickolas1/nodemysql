ceb0b3266332b5dc14e6e97e380d831304fcd5a0
Peter Tountas <petertountas1@gmail.com>
2017-01-11 17:28:54 -0600

    question types

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,33 +2,49 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 
 var connect = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	port: 8889,
-	database: 'bamazon'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 8889,
+    database: 'bamazon'
 });
 
 
-connect.query("Select * FROM products", function(err,res){
-    	console.log("\n Current Product List\n")
-    for(var i=0; i <res.length; i++){
-    	console.log("Product ID: " + res[i].item_id + " | " 
-			    	 +"Product Name: " + res[i].product_name + " | " 
-			    	 + "Dept: " + res[i].department_name +  " | " 
-			    	 + "Price: " + res[i].price + " | " 
-			    	 + "Qty: " +res[i].stock_qty);
+connect.query("Select * FROM products", function(err, res) {
+    console.log("\n Current Product List\n")
+    for (var i = 0; i < res.length; i++) {
+        console.log("Product ID: " + res[i].item_id + " | " +
+            "Product Name: " + res[i].product_name + " | " +
+            "Dept: " + res[i].department_name + " | " +
+            "Price: " + res[i].price + " | " +
+            "Qty: " + res[i].stock_qty);
     };
-    search();
+    selection();
 });
 
 
-var search = function() {
-	inquirer.prompt({
-		name: "product_Id",
-		type: "input",
-		message: "What is the Product ID you would like to bid on? ",
-	}).then(function(answer){
-			console.log("You input Product ID: " + answer.product_Id);
-		});
-	};
+var selection = function() {
+    inquirer.prompt({
+        name: "product_Id",
+        type: "input",
+        message: "What is the Product ID you would like to bid on? ",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }, {
+        name: "qty",
+        type: "input",
+        message: "How many would you like to purchase? ",
+        validate: function() {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }).then(function(answer) {
+        console.log("You input Product ID: " + answer.product_Id);
+    });
+};
