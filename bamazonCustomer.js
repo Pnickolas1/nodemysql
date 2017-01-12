76524d1bd108f167c76d70ee0b801a7a75cf7d1f
Peter Tountas <petertountas1@gmail.com>
2017-01-12 17:00:11 -0600

    updates to input prompt logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,18 @@ var connect = mysql.createConnection({
     database: 'bamazon'
 });
 
+function queryDb(string, opts){
+    return new Promise(function (success,failure) {
+        connect.query(string, opts, function (err, res) {
+            if (err) {
+                failure(err);
+            } else {
+                success(res);
+            }
+        });
+    });
+}
+
 
 connect.query("Select * FROM products", function(err, res) {
     console.log("\n Current Product List\n")
@@ -19,12 +31,23 @@ connect.query("Select * FROM products", function(err, res) {
             "Price: " + res[i].price + " | " +
             "Qty: " + res[i].stock_qty);
     };
-    selection();
+    selection().then(function (res) {
+        // something else
+
+            for(var q= 0; q < res.length;q++){
+                console.log("Item ID: " + res[i].item_id + " | " +
+            "Product Name: " + res[i].product_name + " | " +
+            "Dept: " + res[i].department_name + " | " +
+            "Price: " + res[i].price + " | " +
+            "Qty: " + res[i].stock_qty);
+            }
+                selection();
+    });
 });
 
 
 var selection = function() {
-    inquirer.prompt([{
+    return inquirer.prompt([{
         name: "product_Id",
         type: "input",
         message: "What is the Item ID you would like to bid on? ",
@@ -38,23 +61,14 @@ var selection = function() {
         name: "qty",
         type: "input",
         message: "How many would you like to purchase? ",
-        validate: function() {
+        validate: function(value) {
             if (isNaN(value) === false) {
                 return true;
             }
             return false;
         }
     }]).then(function(answer) {
-        var query = "SELECT * FROM products where item_id ? ";
-        connect.query(query, [answer.start, answer.end]), function (err,res) {
-            for(var q= 0; q < res.length;q++){
-                console.log("Item ID: " + res[i].item_id + " | " +
-            "Product Name: " + res[i].product_name + " | " +
-            "Dept: " + res[i].department_name + " | " +
-            "Price: " + res[i].price + " | " +
-            "Qty: " + res[i].stock_qty);
-            }
-                selection();
-        }
+        var query = "SELECT * FROM products where item_id = ? ";
+        return queryDb(query, [answer.start, answer.end]);
     });
 };
