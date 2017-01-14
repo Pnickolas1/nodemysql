1eb86d66d637e88a2d532fb42e6bcf739859d4f8
Peter Tountas <petertountas1@gmail.com>
2017-01-13 19:40:49 -0600

    added data to MySQL file

--- schema.sql
+++ schema.sql
@@ -9,3 +9,28 @@ price DECIMAL(13,2) NOT NULL,
 stock_qty INT(6) NOT NULL,
 PRIMARY KEY(item_id)
 )
+
+
+
+INSERT INTO products
+	(product_name,department_name,price,stock_qty)
+VALUES
+	('cork','kitchen',.20,400),
+	('wine stopper','kitchen',1.00,500),
+	('vase','kitchen',5.00,1000),
+	('salt/pepper holder','kitchen',2.00,500),
+	('pens','office',.50,150),
+	('staples','office',.10,5000),
+	('pottery','kitchen',50.00,25),	
+	('candles','living',5.00,300),
+	('blanket','living',6.00,450),
+	('pillow','living',2.50,1000),
+	('bed sheets','living',100.00,100),
+	('scissors','office',2.30,300),
+	('beer','food',1.00,400),
+	('phone charger','office',1.25,400),
+	('rug','living',1.75,500),
+	('coffe table','furniture',150.00,500),
+	('couch','furniture',175.00,650),
+	('lazy-boy','furniture',120.00,30);
+	
